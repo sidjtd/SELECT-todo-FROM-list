e8a858438f0bf46dd020b383c7766d4609310e81
SDodson <siddodson@gmail.com>
2016-07-18 19:41:55 -1000

    Postgresql Solved Question 7 and through middle of 8

--- todo_app.sql
+++ todo_app.sql
@@ -1,8 +1,8 @@
 -- 1
-DROP user IF EXISTS michaels;
+DROP user IF EXISTS michael;
 
 --2
-CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker'
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 --CREATE ROLE michael
 
 --3
@@ -14,11 +14,26 @@ CREATE DATABASE todo_app;
 --5
 \c todo_app;
 
---6
+--67
 CREATE TABLE tasks(
-  id serial PRIMARY KEY
+  id serial PRIMARY KEY,
+  title varchar (40) NOT NULL,
+  description char NOT NULL,
+  created_at timestamp NOT NULL,
+  updated_at timestamp NOT NULL,
+  completed boolean NOT NULL
 );
 
+--8
+ALTER TABLE IF EXISTS tasks DROP COLUMN IF EXISTS completed;
+ALTER TABLE IF EXISTS tasks ADD COLUMN completed_at timestamp DEFAULT NULL;
+ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET DEFAULT now();
+
+
+
+--9
+
+--10
 
 
 -- end
