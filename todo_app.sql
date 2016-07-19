5de6c7e25c2e14585e92fa157e45a44235c88b5a
SDodson <siddodson@gmail.com>
2016-07-19 09:02:29 -1000

    Up to Problem 11 Completed

--- todo_app.sql
+++ todo_app.sql
@@ -34,45 +34,47 @@ ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET DEFAULT now();
 ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET NOT NULL;
 --iv.
 INSERT INTO tasks (id, title, description, created_at, updated_at, completed at)
-  VALUES (DEFAULT, 'Study SQL', 'Complete this Exercise','Complete this Exercise.', 'Study PostgreSQL', 'Read all the documentation')
+  VALUES (DEFAULT, 'Study SQL', 'Complete this Exercise','Complete this Exercise.', 'Study PostgreSQL', 'Read all the documentation');
 -- CREATE TABLE task () INHERITS (tasks);
 -- INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this Exercise.', now(), now(), NULL);
 -- INSERT INTO task VALUES (DEFAULT , 'Complete this Exercise.', now(), now(), NULL);
 -- INSERT INTO tasks VALUES (DEFAULT , 'Study PostgreSQL', 'Read all the documentation.', now(), now(), NULL);
 
 --v.
-INSERT INTO tasks (title, description)
-  VALUES ('Study PostgreSQL', 'Read all the documentation')
+-- This doesn't work?
+-- INSERT INTO tasks VALUES (title, description)
+-- ('Study PostgreSQL', 'Read all the documentation');
+INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
 
 --vi.
 SELECT title
   FROM tasks
- WHERE completed_at = NULL;
+  WHERE completed_at IS NULL;
 
 --vii.
-UPDATE tasks SET at completed_at = NULL
+UPDATE tasks SET at completed_at IS NULL
   WHERE title = 'Study SQL';
+--INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this Exercise.', now(), now(), NULL);??
 
 --viii.
 SELECT title, description
   FROM tasks
- WHERE completed_at NULL;
+  WHERE completed_at IS NULL;
+--??? SELECT title, description FROM tasks WHERE completed_at is NULL;
 
 --ix.
 SELECT *
   FROM tasks
-  ORDER BY created_at = NULL;
+  ORDER BY created_at IS NULL;
+--??? SELECT * FROM tasks ORDER BY created_at DESC;
 
 --x.
+INSERT INTO tasks VALUES (DEFAULT , 'mistake 1', 'a test entry.', now(), now(), NULL);
 
 --xi.
+INSERT INTO tasks VALUES (DEFAULT , 'mistake 2', 'another test entry.', now(), now(), NULL);
 
 
-
-
-
-
-
-
+SELECT * FROM tasks;
 -- end
 \c user;
